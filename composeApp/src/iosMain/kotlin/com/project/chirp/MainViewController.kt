1a71035dd66cedd64eaf8c9b919dd2ae2aacc92d
fede-debe <federico.debenedictis33@gmail.com>
2026-03-26 18:26:12 +0100

    Remove boilerplate tests and format code

--- composeApp/src/iosMain/kotlin/com/project/chirp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/project/chirp/MainViewController.kt
@@ -2,4 +2,5 @@ package com.project.chirp
 
 import androidx.compose.ui.window.ComposeUIViewController
 
+@Suppress("ktlint:standard:function-naming")
 fun MainViewController() = ComposeUIViewController { App() }
